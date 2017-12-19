0d9d542ae8470c733e2e07bcc408836fd2e5e8da
YuYeh <a080044976@gmail.com>
2017-12-20 02:17:34 +0800

    update Car.html...

---
+++ js/button.js
@@ -0,0 +1,42 @@
+class Button {
+  constructor(color, name) {
+    this.on = false;
+    this.hsl = color.getHSL();
+    this.obj = this.makeButton(name);
+  }
+
+  get isOn() {
+    return this.on;
+  }
+
+  toggle() {
+    this.on = !this.on;
+    this.update();
+  }
+
+  update() {
+    if (this.on) {
+      this.obj.scale.set (1,0.5,1);
+      this.obj.children[0].material.color.setHSL 
+        (this.hsl.h, 0, 1);
+    } else {
+    	this.obj.scale.set(1,1,1);
+    	this.obj.children[0].material.color.setHSL 
+         (this.hsl.h, this.hsl.s, this.hsl.l);
+    }
+  }  
+
+  makeButton(name) {
+    let mesh = new THREE.Mesh(new THREE.BoxGeometry(3, 2, 2),
+      new THREE.MeshLambertMaterial());
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    let button = new THREE.Object3D();
+    button.add(mesh);
+    mesh.material.color.setHSL (this.hsl.h, this.hsl.s, this.hsl.l)
+    mesh.position.y = 1;
+    button.name = name;  // a string
+    return button;
+  }
+
+}
